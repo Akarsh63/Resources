fe94051566a9d07c91269e229735bc67ead5d7fd
Akarsh63 <akarsh.1@iitj.ac.in>
2023-12-24 11:22:03 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Resources from "./components/Resources";
+
+function App() {
+  return (
+    <div >
+      <Resources />
+    </div>
+  );
+}
+
+export default App;
